f15d63ab4312c679ccfb1976b05d6576c43a4158
sc1061 <852185225@qq.com>
2024-04-29 11:04:02 +0800

    wip

--- app/api/is_working_day/route.ts
+++ app/api/is_working_day/route.ts
@@ -2,7 +2,9 @@ import { isWorkingDay } from "@/calendars/calendars";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-    let is_working_day = isWorkingDay(new Date())
+    let targetDate = request.nextUrl.searchParams.get('date')
+    let date = targetDate ? new Date(targetDate) : new Date()
+    let is_working_day = isWorkingDay(date)
 
 
     return NextResponse.json({is_working_day})
