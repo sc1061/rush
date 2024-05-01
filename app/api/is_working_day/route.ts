5f08b370ae0506dbf0ec44cd23a95339031b7ba5
sc1061 <852185225@qq.com>
2024-05-01 23:58:20 +0800

    Add auto cache

--- app/api/is_working_day/route.ts
+++ app/api/is_working_day/route.ts
@@ -1,9 +1,12 @@
-import { getCurrentCache, isWorkingDay } from "@/calendars/calendars";
+import { cacheCalendars, getCurrentCache, isCalendarsCache, isWorkingDay } from "@/calendars/calendars";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
     let targetDate = request.nextUrl.searchParams.get('date')
     let date = targetDate ? new Date(targetDate) : new Date()
+    if(!isCalendarsCache()) {
+        await cacheCalendars()
+    }
     let is_working_day = isWorkingDay(date)
 
     return NextResponse.json({is_working_day, ...getCurrentCache()})
